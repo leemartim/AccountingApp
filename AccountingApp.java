dd0e3f242827a533e100320116d748857c04464f
leemartim <94498992+leemartim@users.noreply.github.com>
2022-04-12 19:08:08 +0900

    Accounting Argument

--- AccountingApp.java
+++ AccountingApp.java
@@ -5,7 +5,7 @@ public class AccountingApp {
 	public static void main(String[] args) {
 		
 		
-		double valueOfSupply = 12000;
+		double valueOfSupply = Double.parseDouble(args[0]);
         double vatRate = 0.1;
         double expenseRate = 0.3;
         double vat = valueOfSupply * vatRate;
